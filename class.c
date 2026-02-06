79fb190ed66893558ee44e1381313a82842bd3d3
zhouchao03 <jerryzhou.zc@gmail.com>
2026-02-06 10:56:48 +0800

    feat: add AccessFlag.

--- class.c
+++ class.c
@@ -21,6 +21,20 @@ typedef enum ConstantTag {
     CONSTANT_Package            = 20
 } ConstantTag;
 
+typedef enum AccessFlag {
+    ACC_NONE                    = 0x0000,
+    ACC_PUBLIC                  = 0x0001,
+    ACC_PRIVATE                 = 0x0002,
+    ACC_PROTECTED               = 0x0004,
+    ACC_STATIC                  = 0x0008,
+    ACC_FINAL                   = 0x0010,
+    ACC_INTERFACE               = 0x0200,
+    ACC_ABSTRACT                = 0x0400,
+    ACC_SYNTHETIC               = 0x1000,
+    ACC_ANNOTATION              = 0x2000,
+    ACC_ENUM                    = 0x4000
+} AccessFlag;
+
 typedef struct CONSTANT_Utf8_info {
     U2      length;
     char    *bytes;
