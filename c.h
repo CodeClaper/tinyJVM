e72de01767a45f247b257b6d71ebca861a4419b5
zhouchao03 <jerryzhou.zc@gmail.com>
2026-02-06 10:34:38 +0800

    feat: intro c.

---
+++ c.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#define UNUSED(V) ((void) V)
+
+typedef uint8_t     U1;
+typedef uint16_t    U2;
+typedef uint32_t    U4;
+typedef uint64_t    U8;
+
+typedef int8_t      I1;
+typedef int16_t     I2;
+typedef int32_t     I4;
+typedef int64_t     I8;
