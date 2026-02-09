ff413cc3b48893d05dd22cc161c5516feaa717d8
zhouchao03 <jerryzhou.zc@gmail.com>
2026-02-09 23:20:28 +0800

    build: change the Makefile.

--- src/java.c
+++ src/java.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "java.h"
@@ -7,22 +8,37 @@
 
 struct JavaStates javaStates;
 
+static void usage() {
+	fprintf(stderr, "usage: java [-cp classpath] class\n");
+	exit(EXIT_FAILURE);
+}
+
 static void addClassPath(char *classpath) {
     int r = srealloc(javaStates.classes, sizeof(char *) * (javaStates.num_class_path + 1));
     if (r == ERR) seterror("Out of memory");
-    javaStates.class_path[javaStates.num_class_path] = classpath;
+    int s = sstrdup(javaStates.class_path[javaStates.num_class_path], classpath);
+    if (s == ERR) seterror("Out of memory");
     javaStates.num_class_path++;
 }
 
+/* Init java. */
 static void initJava(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "-cp") == 0) {
+            if (++i > argc) usage();
+            else addClassPath(argv[i]);
         }
     }
+    if (javaStates.num_class_path == 0) 
+        addClassPath(".");
+}
+
+static void runJava(int argc, char *argv[]) {
+    loadClass(argv[0]);
 }
 
 int main(int argc, char *argv[]) {
-    UNUSED(argc);
-    UNUSED(argv);
+    initJava(argc, argv);
+    runJava(argc, argv);
     return EXIT_SUCCESS;
 }
--- src/mmr.c
+++ src/mmr.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include "c.h"
 
 int salloc(void *p, size_t size) {
     p = malloc(size);
-    return (p == NULL) ? ERR : OK;
+    if (p == NULL) return ERR;
+    memset(p, 0, size);
+    return OK;
 }
 
 int srealloc(void *p, size_t size) {
@@ -14,7 +15,11 @@ int srealloc(void *p, size_t size) {
 }
 
 int sstrdup(char *p, char *input) {
-    return (p == NULL) ? ERR : OK;
+    size_t size = strlen(input);
+    int r = salloc(p, size + 1);
+    if (r == ERR) return ERR;
+    memcpy(p, input, size);
+    return OK;
 }
 
 int sfree(void *p) {
