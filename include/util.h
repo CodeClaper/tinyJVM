dc0ba603d9f38845e3522ab0c205884e895bf0c4
zhouchao03 <jerryzhou.zc@gmail.com>
2026-02-09 16:21:33 +0800

    feat: add utils.

---
+++ include/util.h
@@ -0,0 +1,2 @@
+
+void seterror(char *format, ...);
---
+++ src/util.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <strings.h>
+#include <unistd.h>
+#include "java.h"
+
+void seterror(char *format, ...) {
+    size_t len;
+    va_list ap;
+
+    /* Calculate the len. */
+    va_start(ap, format);
+    len = vsnprintf(NULL, 0, format, ap);
+    if (len <= 0) {
+        va_end(ap);
+        return;
+    }
+
+    len = len + 1;
+    char message[len];
+    memset(message, 0, len);
+
+    va_start(ap, format);
+    vsnprintf(message, len, format, ap);
+    va_end(ap);
+
+    memcpy(javaServer.error, message, len);
+}
