53e6a03947cb9a019c73d289cd91b997c0f6d621
zhouchao03 <jerryzhou.zc@gmail.com>
2026-02-09 14:09:10 +0800

    feat: add read<N> methods.

---
+++ class.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include "class.h"
+#include "mmr.h"
+
+/* Read count bytes into buf. */
+static int readb(FILE *fp, void *buf, U4 count) {
+    if (fread(buf, 1, count, fp) != count) return ERR;
+    else return OK;
+}
+
+/* Read unsigned integer of size count into *u */
+static int readu(FILE *fp, void *u, U4 count) {
+    U1 b[4];
+
+    TRY(readb(fp, b, count));
+    switch (count) {
+        case 1:
+            *(U1 *)u = b[0];
+            break;
+        case 2:
+            *(U2 *)u = b[0] << 8 | b[1];
+            break;
+        case 4:
+            *(U4 *)u = b[0] << 24 | b[1] << 16 | b[2] << 8 | b[3];
+            break;
+        default: 
+            goto error;
+    }
+    return OK;
+error:
+    return ERR;
+}
+
+/* Read string into buffer. */
+static int reads(FILE *fp, char **s, U2 count) {
+    TRY(salloc(*s, count + 1));
+    TRY(readb(fp, *s, count));
+    (*s)[count] = '\0';
+    return OK;
+error:
+    return ERR;
+}
+
+
+/* Raad the constant pool. */
+static int readcp(FILE *fp, ConstantPoolInfo ***cp, U2 count) {
+    if (count == 0) {
+        cp = NULL;
+        return OK;
+    }
+    TRY(salloc(*cp, sizeof(ConstantPoolInfo *) * count));
+    /* CP starts from 1. */
+    for (int i = 1; i < count; i++) {
+        TRY(salloc(**cp, sizeof(ConstantPoolInfo)));
+        ConstantPoolInfo *current = **cp;
+        TRY(readu(fp, &current->tag, 2));
+        switch (current->tag) {
+            case CONSTANT_Utf8:
+                TRY(readu(fp, &current->info.utf8_info.length, 2));
+                TRY(reads(fp, &current->info.utf8_info.bytes, current->info.utf8_info.length));
+                break;
+            case CONSTANT_Integer:
+                TRY(readu(fp, &current->info.integer_info.bytes, 4));
+                break;
+            case CONSTANT_Float:
+                TRY(readu(fp, &current->info.float_info.bytes, 4));
+                break;
+            case CONSTANT_Long:
+                TRY(readu(fp, &current->info.long_info.high_bytes, 4));
+                TRY(readu(fp, &current->info.long_info.low_bytes, 4));
+                break;
+            case CONSTANT_Double:
+                TRY(readu(fp, &current->info.long_info.high_bytes, 4));
+                TRY(readu(fp, &current->info.long_info.low_bytes, 4));
+                break;
+            case CONSTANT_Class:
+                TRY(readu(fp, &current->info.class_info.name_index, 2));
+                break;
+            case CONSTANT_String:
+                TRY(readu(fp, &current->info.string_info.string_index, 2));
+                break;
+            case CONSTANT_Fieldref:
+                TRY(readu(fp, &current->info.fieldref_info.class_index, 2));
+                TRY(readu(fp, &current->info.fieldref_info.name_type_index, 2));
+                break;
+            case CONSTANT_Methodref:
+                TRY(readu(fp, &current->info.methodref_info.class_index, 2));
+                TRY(readu(fp, &current->info.methodref_info.name_type_index, 2));
+                break;
+            case CONSTANT_InterfaceMethodref:
+                TRY(readu(fp, &current->info.interfacemethodref_info.class_index, 2));
+                TRY(readu(fp, &current->info.interfacemethodref_info.name_type_index, 2));
+                break;
+            case CONSTANT_NameAndType:
+                TRY(readu(fp, &current->info.nametype_info.name_index, 2));
+                TRY(readu(fp, &current->info.nametype_info.descriptor_index, 2));
+                break;
+            case CONSTANT_MethodHandle:
+                TRY(readu(fp, &current->info.methodhandle_info.reference_kind, 1));
+                TRY(readu(fp, &current->info.methodhandle_info.reference_index, 2));
+                break;
+            case CONSTANT_MethodType:
+                TRY(readu(fp, &current->info.methodtype_info.descriptor_index, 2));
+                break;
+            case CONSTANT_Dynamic:
+                TRY(readu(fp, &current->info.dynamic_info.bootstrap_method_attr_index, 2));
+                TRY(readu(fp, &current->info.dynamic_info.name_type_index, 2));
+                break;
+            case CONSTANT_InvokeDyanmic:
+                TRY(readu(fp, &current->info.invokedynamic_info.bootstrap_method_attr_index, 2));
+                TRY(readu(fp, &current->info.invokedynamic_info.name_type_index, 2));
+                break;
+            case CONSTANT_Module:
+                TRY(readu(fp, &current->info.module_info.name_index, 2));
+                break;
+            case CONSTANT_Package:
+                TRY(readu(fp, &current->info.package_info.name_index, 2));
+                break;
+            default: goto error;
+        }
+    }
+error:
+    return ERR;
+}
+
+static int readClass(FILE *fp, ClassFile *class) {
+    TRY(readu(fp, &class->magic, 4));
+    TRY(readu(fp, &class->major_version, 2));
+    TRY(readu(fp, &class->minor_version, 2));
+    TRY(readu(fp, &class->constant_pool_count, 2));
+    TRY(readcp(fp, &class->constant_pool, class->constant_pool_count));
+	TRY(readu(fp, &class->access_flags, 2));
+	TRY(readu(fp, &class->this_class, 2));
+	TRY(readu(fp, &class->super_class, 2));
+	TRY(readu(fp, &class->interfaces_count, 2));
+    return OK;
+error:
+    return ERR;
+}
+
+static int checkClass(ClassFile *class) {
+    if (class->magic != MAGIC) return ERR;
+    return OK;
+}
+
+ClassFile *loadClass(char *class_name) {
+
+}
