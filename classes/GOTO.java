46ca46b02a89f3bee638a6459a201fafe989096d
zhouchao03 <jerryzhou.zc@gmail.com>
2026-02-24 11:11:42 +0800

    test: add more class test.

---
+++ classes/GOTO.java
@@ -0,0 +1,6 @@
+public class GOTO {
+    public void testGOTO() {
+        int i = 0;
+        while (i < 10) i++;
+    }
+}
---
+++ classes/IINC.java
@@ -0,0 +1,8 @@
+public class IINC {
+     public void testIinc() {
+        int count = 10;
+        count++;      // 产生 iinc
+        count += 5;   // 产生 iinc
+        count--;      // 产生 iinc
+    }
+}
