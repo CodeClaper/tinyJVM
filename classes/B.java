bffafd2c614e1be395aac60d9d0c9bd9f710b6c2
zhouchao03 <jerryzhou.zc@gmail.com>
2026-02-24 10:20:30 +0800

    test: make classes test.

--- classes/B.java
+++ classes/B.java
@@ -14,6 +14,8 @@ public class B extends A  {
 
     @Override
     public void sayHello() {
+        this.a++;
+        this.b++;
         System.out.println("Say Hello from: " + this.name);
     }
 
