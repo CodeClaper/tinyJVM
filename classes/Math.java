4f98f2944caa3950b3e5452090baee70250cf2f8
zhouchao03 <jerryzhou.zc@gmail.com>
2026-02-27 15:47:29 +0800

    test: add test.

--- classes/Math.java
+++ classes/Math.java
@@ -6,4 +6,9 @@ public class Math {
     public int substract(int a, int b) {
         return a - b;
     }
+
+    public int avg(int a, int b) {
+        int sum = a + b;
+        return sum / 2;
+    }
 }
