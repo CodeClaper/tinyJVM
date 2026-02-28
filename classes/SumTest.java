e3ceb2e384abf55e7d88b78a3cacb6a645ba3f7b
zhouchao03 <jerryzhou.zc@gmail.com>
2026-02-28 10:02:24 +0800

    test: add more test class.

---
+++ classes/SumTest.java
@@ -0,0 +1,12 @@
+public class SumTest {
+    private static int calc() {
+        int a = 100;
+        int b = 200;
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int c = calc();
+        System.out.println("cals: " + c);
+    }
+}
