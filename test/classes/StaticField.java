f3bad11ff16e9b730073ea940e2c7c3a88ff100c
zhouchao03 <jerryzhou.zc@gmail.com>
2026-03-18 15:33:06 +0800

    test: change test class.

--- test/classes/StaticField.java
+++ test/classes/StaticField.java
@@ -1,9 +1,10 @@
-public class StaticFeild {
+public class StaticField {
     private int a;
     private char b;
     private static int c = 3;
 
     public static void main(String[] args) {
         int d = c + 1;
+        System.out.println("d: " + d);
     }
 }
