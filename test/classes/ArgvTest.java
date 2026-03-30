a8cacfccb4a280cd2e209168ba9a4455debd52bb
zhouchao03 <jerryzhou.zc@gmail.com>
2026-03-30 10:00:00 +0800

    test: add more test classes.

--- test/classes/ArgvTest.java
+++ test/classes/ArgvTest.java
@@ -1,7 +1,7 @@
 
 public class ArgvTest {
     public static void main(String[] args) {
-        System.out.println("Args 1: " + args[0]);
-        System.out.println("Args 2: " + args[1]);
+        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
 }
